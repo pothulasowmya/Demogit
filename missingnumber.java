4b66a95a8bcf8f861df834e6a199de01633ad736
pothulasowmya <84896000+pothulasowmya@users.noreply.github.com>
2023-02-09 15:15:13 +0530

    Add files via upload

---
+++ missingnumber.java
@@ -0,0 +1,21 @@
+public class missingnumber{
+
+    public static void main(String[] args) {
+ 
+        int[] arr1={1,2,4,5,6};
+        System.out.println(+missingNumber(arr1));
+    }
+ 
+    public static int missingNumber(int[] arr)
+    {
+        int n=arr.length+1;
+        int sum=n*(n+1)/2;
+        int restSum=0;
+        for (int i = 0; i < arr.length; i++) {
+            restSum+=arr[i];
+        }
+        int missingNumber=sum-restSum;
+        return missingNumber;
+    }
+}
+ 
